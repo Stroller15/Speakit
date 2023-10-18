2a04664e3bb149f011c51062ec5e48b87b4eccc4
Stroller15 <shubhamverma6351@gmail.com>
2023-10-18 12:43:06 +0530

    feat: :sparkles: add navabar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import Navbar from '@/components/Navbar'
+
+
 
 export const metadata = {
   title: 'Opinex',
   description: 'A Reddit type application built with Next.js and TypeScript.',
 }
+
+const inter = Inter({subsets: ['latin']})
 
 export default function RootLayout({
   children,
@@ -11,8 +18,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html suppressHydrationWarning lang='en' className={cn('bg-white text-slate-900 antialiased light',
+    inter.className
+    )}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+      
+        <Navbar/>
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+        {children}
+        </div>
+      
+
+      </body>
     </html>
   )
 }
